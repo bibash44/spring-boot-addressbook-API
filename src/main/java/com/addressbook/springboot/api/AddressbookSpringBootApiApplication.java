efdc99f40d2fed51cd573b262858508fac5f917a
Bibash kattel <bibashkatel4@gmail.com>
2023-02-28 17:47:03 +0000

    Initializing the project and installing maven dependencies

---
+++ src/main/java/com/addressbook/springboot/api/AddressbookSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.addressbook.springboot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressbookSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addressbook/springboot/api/AddressbookSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressbook.springboot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressbookSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
